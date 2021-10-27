3f4b7d964e428b66ca05eb7ae09b65e38c0dd21b
JacobG03 <jacobgluszek03@gmail.com>
2021-10-27 19:14:08 +0200

    register, login added #no security yet

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,10 @@
-from app import app
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import app, db
+from app.models import User
+from flask import request, jsonify, make_response
+import jwt
+import datetime
+from app.decorators import token_required
 
 
 @app.get('/api')
@@ -9,8 +15,50 @@ def api():
   }
 
 #! get /api/user    (current_user) (:user === some other user)
-#? post /api/user/login
+
+
+
 #? post /api/user/register
+
+@app.post('/api/user/register')
+def register():
+  data = request.get_json()
+  hashed_password = generate_password_hash(data['password'], method='sha256')
+
+  user = User(
+    name=data['name'],
+    surname=data['surname'],
+    email=data['email'],
+    password=hashed_password,
+  )
+
+  db.session.add(user)
+  db.session.commit()
+
+  return jsonify({
+    'success': True,
+    'message': 'Registered successfully'
+  })
+
+
+#? post /api/user/login
+@app.post('/api/user/login')
+def login():
+  data = request.get_json()
+  if not data or not data['email'] or not data['password']:
+    return make_response('could not verify', 401, {'Authentication': 'login required"'})   
+
+  user = User.query.filter_by(email=data['email']).first()  
+  if check_password_hash(user.password, data['password']):
+    token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=45)}, app.config['SECRET_KEY'], "HS256")
+
+    return jsonify({'token' : token})
+
+  return make_response('could not verify',  401, {'Authentication': '"login required"'})
+
+
+
+
 #* get /api/:user     (:user or :id)
 #* /api/:user/profile
 #* /api/:user/history   etc..
@@ -21,6 +69,14 @@ def api():
 
 
 #! get /api/users     returns all /users routes
+
+@app.get('/api/users')
+@token_required
+def users(current_user):
+  return jsonify({
+    'message': 'Working'
+  })
+
 #* get /api/users/1/500  default by id  (500 being the limit)
 #* get /api/users/1/500?sort=new   // newest users
 #* get /api/users/1/500?sort=old   // oldest users    etc..
